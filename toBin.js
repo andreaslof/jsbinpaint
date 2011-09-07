27b3c35c5a876fbe6869143bdcaaf3ab8e8228fb
Andreas Löf Hermansson <a@andreaslof.se>
2011-09-07 16:51:39 +0200

    moved the toBin()-function into its own file

---
+++ toBin.js
@@ -0,0 +1,13 @@
+String.prototype.toBin = function(){
+	var st,i,j,d;
+	var arr = [];
+	var len = this.length;
+	for (i = len; i>0; i--){
+	 	d = this.charCodeAt(i-1);
+		for (j = 0; j < 8; j++) {
+	 		arr[arr.length] = d%2;
+			d = Math.floor(d/2);
+		}
+	}
+	return arr.reverse().join("");
+}
