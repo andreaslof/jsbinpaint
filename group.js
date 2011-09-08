734c31a48535b8696f75521fcb9983d7bcd47b1c
Andreas Löf Hermansson <a@andreaslof.se>
2011-09-08 22:09:04 +0200

    Editing group.js since the while-loop disappeared

--- group.js
+++ group.js
@@ -2,14 +2,17 @@ String.prototype.group = function() {
 	var i, j;
 	var res = [];
 	var str = this;
-	var len = str.length/4;
-	for(i = len; i >= 0; i--){
-		res[i] = {
-			y1:str.charAt(i),
-			x1:str.charAt(i+1),
-			y2:str.charAt(i+2),
-			x2:str.charAt(i+3)
-		};
+	while (str.length >= 255) {
+		var len = str.length/4;
+		for (i = len; i >= 0; i--) {
+			res[i] = {
+				x1:str.charAt(i),
+				y1:str.charAt(i+1),
+				x2:str.charAt(i+2),
+				y2:str.charAt(i+3)
+			};
+		}
+		return res;
 	}
-	return res;
+	return;
 }
