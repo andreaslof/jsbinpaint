549936e9ce641b56b905217611cacc05fa9c6de1
Simon Westerlund <s@simonwesterlund.se>
2011-09-10 23:27:25 +0200

    Here we go, cleaned up the code, commented, did make the paint scale and center and added support for time logging

--- binpaint.js
+++ binpaint.js
@@ -2,7 +2,6 @@
 // An experiment which will paint with hashed-base2-encoded strings
 // Author(s): Andreas Löf Hermansson & Simon Westerlund
 
-
 function prepareString()
 {
 	var string = document.getElementById('string').value.md5().toBin().group();
@@ -10,18 +9,29 @@ function prepareString()
 }
 
 function paintFromArray(array){
-	var canvas = document.getElementById('canvas');
-	var context = canvas.getContext('2d');
+
+	var extend			=	40,
+		margin			=	50,
+		lineWidth		=	1,
+		logTime			=	false;
+
+	var canvas			=	document.getElementById('canvas'),
+		context			=	canvas.getContext('2d'),
+		startTime		=	new Date().getTime(),
+		lastPosition	=	[0, 0],
+		coordinates		=	[0, 0],
+		paths			=	[],
+		maxX			=	0,
+		maxY			=	0,
+		minX			=	0,
+		minY			=	0;
+
 	canvas.setAttribute('width', document.body.offsetWidth);
 	canvas.setAttribute('height', document.body.offsetHeight);
-	var extend = 40;
 	context.lineWidth = 1;
-	var lastPosition = [canvas.width / 2, canvas.height / 2];
-	var coordinates = [0, 0];
-	var paths = [];
 
-	for (var i in array)
-	{
+	for (var i in array){
+
 		coordinates[0] = lastPosition[0];
 		coordinates[1] = lastPosition[1];
 
@@ -44,12 +54,63 @@ function paintFromArray(array){
 
 		lastPosition[0] = coordinates[0];
 		lastPosition[1] = coordinates[1];
+
+		if(maxX < coordinates[0])
+			maxX = coordinates[0];
+		if(maxY < coordinates[1])
+			maxY = coordinates[1];
+		if(minX > coordinates[0])
+			minX = coordinates[0];
+		if(minY > coordinates[1])
+			minY = coordinates[1];
+
 	}
 
+
+	// if 0, make it positive
+	if(minX < 0)
+		minX = 0 - minX;
+	if(minY < 0)
+		minY = 0 - minY;
+
+
+	// add the new positive value to maxX and maxY
+	maxX += minX;
+	maxY += minY;
+
+	var scale = 0;
+
+
+	// find the ratio of the "painting"
+	if(canvas.width / maxX < canvas.height / maxY)
+		scale = (canvas.width - margin) / maxX;
+	else
+		scale = (canvas.height - margin) / maxY;
+
+
+	// scale the canvas, so that we don't have to recalculate padding and so on...
+	context.scale(scale, scale);
+
+
+	// the letter e draws a small painting
+	// calculate ratios
+	var ofX = (canvas.width / 2) - ((maxX * scale) / 2),
+		ofY = (canvas.height / 2) - ((maxY * scale) / 2);
+
+
+	// move all content to the center, to all the party
+	context.translate(minX + (ofX / scale), minY + (ofY / scale));
+
+
+	// and...paint!
 	for (var i in paths){
 		context.beginPath();
+		// context.moveTo((paths[i][0][0] + minX + (ofX / scale)), paths[i][0][1] + minY + (ofY / scale));
+		// context.lineTo((paths[i][1][0] + minX + (ofX / scale)), paths[i][1][1] + minY + (ofY / scale));
 		context.moveTo(paths[i][0][0], paths[i][0][1]);
 		context.lineTo(paths[i][1][0], paths[i][1][1]);
 		context.stroke();
 	}
+
+	if(logTime)console.log('elapsed time: ' + (new Date().getTime() - startTime) + 'ms');
 }
